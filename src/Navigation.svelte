00c82a744923c6d5ed0d27eebd167e82a0cf60b1
mhigley <mhigley@bitwiseindustries.com>
2023-02-26 16:20:42 -0800

    simple css tests

---
+++ src/Navigation.svelte
@@ -0,0 +1,33 @@
+<nav>
+    <ul>
+        <li>One</li>
+        <li>Second Link</li>
+        <li>Third is the Word</li>
+        <li>4</li>
+        <li>Number Five is Alive</li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        width: 100%;
+    }
+    ul {
+        width: 100%;
+        display: flex;
+        padding: 0;
+        scroll-snap-type: x mandatory;
+        overflow-x: scroll;
+    }
+    li {
+        font-family: sans-serif;
+        font-size: 0.8rem;
+        color: white;
+        display: inline-block;
+        white-space: nowrap;
+        background: hsl(200, 95%, 83%);
+        padding: 5px 4rem;
+        border-radius: 100px;
+        scroll-snap-align: start;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import Navigation from '../Navigation.svelte';
+</script>
+
+<Navigation />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
